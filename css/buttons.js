8777b244e698473f3dffbef1f54480e623baf1a1
S1L3-Music <102176249+S1L3-Music@users.noreply.github.com>
2022-03-22 17:09:12 +0000

    Add files via upload

---
+++ css/buttons.js
@@ -0,0 +1,13 @@
+function mouseOverImage() {
+        document.getElementById("ImageButton1").src = "icons/button_pressed.png";
+		document.getElementById("ImageButton2").src = "icons/button_pressed.png";
+		document.getElementById("ImageButton3").src = "icons/button_pressed.png";
+		document.getElementById("ImageButton4").src = "icons/button_pressed.png";
+       }
+
+function mouseOutImage() {
+        document.getElementById("ImageButton1").src = "icons/button_normal.png";
+		document.getElementById("ImageButton2").src = "icons/button_normal.png";
+		document.getElementById("ImageButton3").src = "icons/button_normal.png";
+		document.getElementById("ImageButton4").src = "icons/button_normal.png";
+       }
